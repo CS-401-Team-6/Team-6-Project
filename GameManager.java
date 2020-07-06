19f0da43dd60aba5175ffba125adfd95c169e132
Guodong Lu <lu172894713@gmail.com>
2020-07-05 23:44:45 -0700

    Adding GameManager, GameUserInterface, and UserLogin_GUI

---
+++ GameManager.java
@@ -0,0 +1,12 @@
+
+public class GameManager {
+
+	public static void main(String[] args) {
+		// Use file or array to store user's information?
+		GameUserInterface Interface;
+		
+		Interface = new userLogin_GUI();
+		Interface.execute();
+	}
+
+}
