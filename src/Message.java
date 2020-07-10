0f9447df09e62b34b6a9245c3901316334102412
Angelo Rosario <arosario7@horizon.csueastbay.edu>
2020-07-09 20:58:41 -0700

    Added really rough base for message and messageProcessor classes. -Angelo

---
+++ src/Message.java
@@ -0,0 +1,47 @@
+import java.io.Serializable;
+
+public class Message implements Serializable{
+	//Messages will be defined as actions to be sent between client and server
+	//Depending on the type of message, a different action will be processed through MessageProcessor
+	
+    protected String type;
+    protected String status;
+    protected String text;
+    
+    public Message(){
+        this.type = "Undefined";
+        this.status = "Undefined";
+        this.text = "Undefined";
+    }
+    
+    public Message(String type, String status, String text){
+    	this.type = type;
+    	this.status = status;
+    	this.text = text;
+    }
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+    
+}
---
+++ src/MessageProcessor.java
@@ -0,0 +1,13 @@
+
+public class MessageProcessor {
+	//This class will take in a message and depending on the type,
+	//process will perform different actions within the server
+	//A message object will be returned back
+	
+	public Message process(Message message) {
+		/* If status == hit, stand/hold, double down, split
+		 * perform specific action
+		 */
+		return message;
+	}
+}
