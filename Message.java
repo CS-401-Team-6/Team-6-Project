5ee4cdd0775e7d265b4060a540c2e6d1ff8a8ceb
Angelo Rosario <arosario7@horizon.csueastbay.edu>
2020-07-11 14:55:17 -0700

    Skeleton for Message and MessageProcessor. -Angelo

--- Message.java
+++ Message.java
@@ -1,8 +1,14 @@
 import java.io.Serializable;
 
 public class Message implements Serializable{
-	//Messages will be defined as actions to be sent between client and server.
-	//Depending on the type of message, a different action will be processed through MessageProcessor.
+	//Messages will be defined as actions to be sent between client and server
+	//Depending on the type of message, a different action will be processed through MessageProcessor
+	
+	//Need to possibly get these objects either from passing it in through the message directly
+	//or do it through the server and grab it through a server-side class, then pass it in through messageProcessor instead.
+	//protected User user; //Need to get player hand
+	//protected Deck deck; //Need to get the deck
+	//protected Game game; //Need to get the game if wanted to go with the path of ONLY passing in a Game object
 	
     protected String type;
     protected String status;
--- MessageProcessor.java
+++ MessageProcessor.java
@@ -1,13 +1,38 @@
-
 public class MessageProcessor {
 	//This class will take in a message and depending on the type,
 	//process will perform different actions within the server
-	//A message object will be returned back.
+	//A message object will be returned back, possibly more...
 	
-	public Message process(Message message) {
-		/* If status == hit, stand/hold, double down, split
-		 * perform specific action
-		 */
+	//EDIT: Might change return variable to void and just send directly back using ObjectOutputStream
+	//EDIT: Instead of passing in User and Deck, we can also pass in Game instead, however
+			//we would have to know which player to draw the deck for.
+	public Message process(Message message, User user, Deck deck) {
+        // get the input stream from the connected socket
+        // create an ObjectInputStream so we can read data from it.
+		//EDIT: These will either be passed in as a parameter or created here.
+		if (message.type.equals("HIT")) {
+			//Draw a card from the deck for the player
+			user.hand.hit();
+			message.setStatus("Success");
+			//EDIT: Use ObjectOutputStream to send objects back.
+		}
+		else if (message.type.equals("DOUBLE DOWN")) {
+			//Double the player's bet amount, draw a card and place in player's hand
+			//EDIT: Double player bet amount
+			user.hand.hit();
+			message.setStatus("Success");
+			//EDIT: Use ObjectOutputStream to send objects back.
+		}
+		else if (message.type.equals("SPLIT")) {
+			//Should already be checked if split is available back in client
+			//Create a new hand by sending one of the cards over and drawing a second card for each hand
+			//The betting amount for the second hand will be equal to the starting bet
+		}
+		else if (message.type.equals("STAND")) {
+			//Do nothing
+		}
+		else
+			System.out.println("INVALID ACTION");
 		return message;
 	}
 }
