103b0d384f2c6246136b2e30e441cab87c80e07e
Michael Maddux <mmaddux@horizon.csueastbay.edu>
2020-07-15 15:22:20 -0700

    "GUI And Server Integration in Dev Branch"

--- Message.java
+++ Message.java
@@ -1,53 +1,49 @@
+package client;
 import java.io.Serializable;
 
-public class Message implements Serializable{
-	//Messages will be defined as actions to be sent between client and server
-	//Depending on the type of message, a different action will be processed through MessageProcessor
-	
-	//Need to possibly get these objects either from passing it in through the message directly
-	//or do it through the server and grab it through a server-side class, then pass it in through messageProcessor instead.
-	//protected User user; //Need to get player hand
-	//protected Deck deck; //Need to get the deck
-	//protected Game game; //Need to get the game if wanted to go with the path of ONLY passing in a Game object
-	
-    protected String type;
-    protected String status;
+public class Message implements Serializable {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	protected String type;
+    protected byte[] status;
     protected String text;
-    
+
     public Message(){
         this.type = "Undefined";
-        this.status = "Undefined";
+ 
         this.text = "Undefined";
     }
-    
-    public Message(String type, String status, String text){
+
+    public Message(String type, byte[] status, String text){
+       setType(type);
+       setStatus(status);
+       setText(text);
+    }
+
+    private void setType(String type){
     	this.type = type;
+    }
+
+    public void setStatus(byte[] status){
     	this.status = status;
+    }
+
+    public void setText(String text){
     	this.text = text;
     }
 
-	public String getType() {
-		return type;
-	}
+    public String getType(){
+    	return type;
+    }
 
-	public void setType(String type) {
-		this.type = type;
-	}
+    public byte[] getStatus(){
+    	return status;
+    }
 
-	public String getStatus() {
-		return status;
-	}
+    public String getText(){
+    	return text;
+    }
 
-	public void setStatus(String status) {
-		this.status = status;
-	}
-
-	public String getText() {
-		return text;
-	}
-
-	public void setText(String text) {
-		this.text = text;
-	}
-    
 }
